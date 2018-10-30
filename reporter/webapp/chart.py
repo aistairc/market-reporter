7a9fa1607828e5f0882779c7522c473fe89fa277
Fumiya Yamamoto <yamamoto-fymiya10@aist.go.jp>
2018-10-30 11:24:24 +0900

    Apply flake8 (#20)

--- reporter/webapp/chart.py
+++ reporter/webapp/chart.py
@@ -4,7 +4,6 @@ from itertools import groupby
 
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql import text
-from sqlalchemy.types import DateTime
 
 from reporter.database.read import fetch_prices_of_a_day
 from reporter.util.constant import UTC
@@ -40,32 +39,34 @@ def fetch_points(session: Session, ric: str, start: datetime, end: datetime) -> 
 
 
 def _xs_ys_of_group(iter: Iterable[Tuple[float, float, float]]) -> Dict[str, List[float]]:
-    result = { 'xs': [], 'ys': [] }
+    result = {'xs': [], 'ys': []}
     for t, val, _ in iter:
         result['xs'].append(t)
         result['ys'].append(val)
     return result
 
+
 def fetch_all_points_fast(session: Session, rics: List[str], start: datetime, end: datetime) -> Dict[str, float]:
     sql = text("""
-        SELECT EXTRACT(epoch FROM t) AS t, val ::float, ric
-        FROM
-        (SELECT generate_series(:start ::timestamp, :end ::timestamp, '5 minutes' ::interval) AS t) times
-        CROSS JOIN
-        (SELECT * FROM (VALUES
-        """ +
-        ", ".join(["(:ric%d)" % i for i in range(len(rics))])
-        + """
-        ) AS ric_vals (ric)) rics
-        NATURAL LEFT JOIN prices
-        ORDER BY ric ASC, t ASC
-    """)
-    ric_dict = { "ric%d" % i: ric for i, ric in enumerate(rics) }
+               SELECT EXTRACT(epoch FROM t) AS t, val ::float, ric
+               FROM
+               (SELECT generate_series(:start ::timestamp, :end ::timestamp, '5 minutes' ::interval) AS t) times
+               CROSS JOIN
+               (SELECT * FROM (VALUES
+               """ +
+               ", ".join(["(:ric%d)" % i for i in range(len(rics))])
+               + """
+               ) AS ric_vals (ric)) rics
+               NATURAL LEFT JOIN prices
+               ORDER BY ric ASC, t ASC
+               """)
+    ric_dict = {"ric%d" % i: ric for i, ric in enumerate(rics)}
     result = session.bind.execute(sql, start=start, end=end, **ric_dict)
     result = {
         ric: _xs_ys_of_group(g) for ric, g in groupby(result, lambda e: e[2])
     }
     return result
+
 
 def fetch_close(session: Session, ric: str, jst: datetime) -> float:
     result = session \
@@ -74,20 +75,21 @@ def fetch_close(session: Session, ric: str, jst: datetime) -> float:
         .scalar()
     return float(result) if result is not None else None
 
+
 def fetch_all_closes_fast(session: Session, rics: List[str], start: datetime, end: datetime) -> Dict[str, float]:
     sql = text("""
-        SELECT prices.ric, prices.val ::float
-        FROM
-        (SELECT * FROM (VALUES
-        """ +
-        ", ".join(["(:ric%d)" % i for i in range(len(rics))])
-        + """
-        ) AS ric_vals (ric)) rics
-        NATURAL JOIN prices
-        NATURAL LEFT JOIN closes
-        WHERE closes.t >= :start AND closes.t < :end
-    """)
-    ric_dict = { "ric%d" % i: ric for i, ric in enumerate(rics) }
+               SELECT prices.ric, prices.val ::float
+               FROM
+               (SELECT * FROM (VALUES
+               """ +
+               ", ".join(["(:ric%d)" % i for i in range(len(rics))])
+               + """
+               ) AS ric_vals (ric)) rics
+               NATURAL JOIN prices
+               NATURAL LEFT JOIN closes
+               WHERE closes.t >= :start AND closes.t < :end
+               """)
+    ric_dict = {"ric%d" % i: ric for i, ric in enumerate(rics)}
     result = session.bind.execute(sql, start=start, end=end, **ric_dict)
     result = dict(list(result))
     return result
