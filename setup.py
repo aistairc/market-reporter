6a093976f4694fa10a279df7943ce71c819bc4f2
Miyazawa Akira <pecorarista@gmail.com>
2019-10-24 14:09:07 +0900

    Use pipenv

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import find_packages, setup
-
-import reporter
+from setuptools import setup
 
 
-setup(name='market-reporter',
-      version=reporter.__version__,
-      description='Market Reporter',
-      author='Akira Miyazawa, Tatsuya Aoki, Fumiya Yamamoto, Soichiro Murakami, and Akihiko Watanabe',
-      author_email='miyazawa-a@nii.ac.jp, aoki@lr.pi.titech.ac.jp',
-      url='https://github.com/aistairc/market-reporter',
-      packages=find_packages(exclude=['docs', 'envs', 'figures', 'resources', 'tests']),
-      test_suite='tests')
+setup()
